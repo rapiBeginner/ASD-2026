348171cdce6952872bac4b097987eb057afa7a65
rapiBeginner <araffi968@gmail.com>
2026-05-03 13:09:52 +0700

    percobaan2pertanyaan

--- jobsheet9/StackTugasMahasiswa01.java
+++ jobsheet9/StackTugasMahasiswa01.java
@@ -85,7 +85,7 @@ public class StackTugasMahasiswa01 {
 
     String konversiDesimalKeBiner(int nilai) {
         StackKonversi01 stack = new StackKonversi01();
-        while (nilai > 0) {
+        while (nilai != 0) {
             int sisa = nilai % 2;
             stack.push(sisa);
             nilai = nilai / 2;
